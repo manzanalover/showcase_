d90e97e55f89af9ec7f1dbb8d697b76c761247d9
Iuri Silva <iuriprogramacion@gmail.com>
2022-09-28 12:59:47 +0200

    añadido valor igual a cero creditos iniciales

--- js/main.js
+++ js/main.js
@@ -1,50 +1,52 @@
+
+
 let totalReciclaje = document.getElementById("totalReciclaje");
 let creditosIniciales = 0;
 totalReciclaje.innerHTML = `${creditosIniciales}`;
 
 let objetos = [
-{
-    id: "botella-agua",
-    creditos: 100
-},
-{
-    id: "botella-vino",
-    creditos: 150
-},
-{
-    id: "papelhigienico",
-    creditos: -80
-},
-{
-    id: "cerveza",
-    creditos: 80
-},
-{
-    id: "periodico",
-    creditos: 90
-},
-{
-    id: "espejo",
-    creditos: -100
-},
-{
-    id: "bombilla",
-    creditos: -120
-},
-{
-    id: "cinta",
-    creditos: -95
-},
-{
-    id: "belen",
-    creditos: 900
-}
+
+    {
+        id: "botella-agua",
+        creditos: 100
+    },
+    {
+        id: "botella-vino",
+        creditos: 150
+    },
+    {
+        id: "papelhigienico",
+        creditos: -80
+    },
+    {
+        id: "cerveza",
+        creditos: 80
+    },
+    {
+        id: "periodico",
+        creditos: 90
+    },
+    {
+        id: "espejo",
+        creditos: -100
+    },
+    {
+        id: "bombilla",
+        creditos: -120
+    },
+    {
+        id: "cinta",
+        creditos: -95
+    },
+    {
+        id: "belen",
+        creditos: 900
+    }
 ];
 
 const drag = (ev) => {
     ev.dataTransfer.setData("text", ev.target.id);
 
-    // console.log("arrastrando...", ev.target.id);
 };
 
 const allowDrop = (ev) => {
@@ -54,26 +56,31 @@ const allowDrop = (ev) => {
 const drop = (ev) => {
     ev.preventDefault();
     let data = ev.dataTransfer.getData("text");
-   
+
 
     let objetoDeseado = objetos.find(objeto => {
         return objeto.id == data
     });
 
     creditosIniciales += objetoDeseado.creditos;
+    
 
-    totalReciclaje.innerHTML = `${creditosIniciales}`;
+    totalReciclaje.innerHTML = `${creditosIniciales} creditos`;
 };
 
 
 let variableGlobal = 0;
+
 let carrito = document.getElementById("totalReciclaje");
 
-const vaciar = () => {
-    variableGlobal = 0;
 
-    carrito.innerHTML = `${variableGlobal}`;
+const vaciar = () => {
+
+    let variableGlobal = 0;
+    carrito.innerHTML = `${variableGlobal} creditos`;
+    creditosIniciales = 0;
 };
+
 
 
 
